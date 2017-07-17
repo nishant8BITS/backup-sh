90acfee1e8d4ac7d0471a235333d1cb35d0940a2
Nishant Kumar <nishant.kumar@etouches.com>
2017-07-17 12:11:40 +0530

    Added Read Line Inpout Format

--- main.sh
+++ main.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
-OF=/Volumes/download-backup-$(date +%Y%m%d).zip
-IF=/Users/$user_name/Downloads 
-zip -r -X $OF $IF
+echo Please, enter path for input folder
+read IF
+
+echo Please, enter backup folder path for input folder
+read OF
+
+if [ -n "$IF" ]; then 
+	OF=$OF/download-backup-$(date +%Y-%m-%d).zip
+	zip -r -X $OF $IF
+	echo Zip Done
+else 
+	echo Sorry you have\'t secified input Folder path.
+fi
